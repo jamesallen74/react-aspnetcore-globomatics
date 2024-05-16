4c8baa584727671390a60c44b996688b462ba366
James Allen <jamesallen74@gmail.com>
2024-05-15 21:36:34 -0400

    added header to reactweb

---
+++ ReactWeb/src/main/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Providing houses all over the world"></Header>
+    </div>
+  );
+}
+
+export default App;
